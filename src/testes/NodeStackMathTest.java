60342934aa38ee26954e991a5f6912a66d879490
ginyusquad <78888093+ginyusquad@users.noreply.github.com>
2021-03-14 01:52:51 -0300

    Correcao ortografica

--- src/testes/NodeStackMathTest.java
+++ src/testes/NodeStackMathTest.java
@@ -12,12 +12,12 @@ public class NodeStackMathTest {
 
 	@Test
 	public void test() {
-		// necessario a classe Util para converter em Charater para a pinha de nodes
-		// Já que a programação generica não quer aceitar o tipo primitivo
+		// necessario a classe Util para converter char[] em Charater[] para a pilha de nodes
+		// JÃ¡ que a programaÃ§Ã£o generica nÃ£o quer aceitar o tipo primitivo
 		// char[]
 		Character[] textoCorreto0 = Util.stringToCharaterlist("(1+2)(8(14+89)){2/7(7*3[5+9(8*9)])}+1");
 		Character[] textoCorreto1 = Util.stringToCharaterlist("(1(2(3*1)(5+8(2))9{([(1)2]3)4}5)6)+7");
-		Character[] textoCorreto2 = Util.stringToCharaterlist("[(5+x)/4–2*(y+z)]");
+		Character[] textoCorreto2 = Util.stringToCharaterlist("[(5+x)/4â€“2*(y+z)]");
 		Character[] textoCorreto3 = Util.stringToCharaterlist("[({})]");
 		Character[] textoCorreto4 = Util.stringToCharaterlist("[]");
 		
@@ -29,53 +29,53 @@ public class NodeStackMathTest {
 		
 		Stack<Character> pilhaMath = new NodeStackMath<Character>();
 		
-		// 1° Teste com Correto 
+		// 1Â° Teste com Correto 
 		pilhaMath.addArray(textoCorreto0);
 		assertTrue(" Texto correto! ", pilhaMath.verifyMath());
 		pilhaMath.clear();
-		// 2° Teste com Correto 
+		// 2Â° Teste com Correto 
 		pilhaMath.addArray(textoCorreto1);
 		assertTrue(" Texto correto! ", pilhaMath.verifyMath());
 		pilhaMath.clear();
 		
-		// 3° Teste com Correto 
+		// 3Â° Teste com Correto 
 		pilhaMath.addArray(textoCorreto2);
 		assertTrue(" Texto correto! ", pilhaMath.verifyMath());
 		pilhaMath.clear();
 		
-		// 4° Teste com Correto 
+		// 4Â° Teste com Correto 
 		pilhaMath.addArray(textoCorreto3);
 		assertTrue(" Texto correto! ", pilhaMath.verifyMath());
 		pilhaMath.clear();
 		
-		// 5° Teste com Correto 
+		// 5Â° Teste com Correto 
 		pilhaMath.addArray(textoCorreto4);
 		assertTrue(" Texto correto! ", pilhaMath.verifyMath());
 		pilhaMath.clear();
 		
 		
 		
-		// 1° Teste com Falso 
+		// 1Â° Teste com Falso 
 		pilhaMath.addArray(textoIncorreto0);
 		assertFalse(" Texto correto! ", pilhaMath.verifyMath());
 		pilhaMath.clear();
 		
-		// 2° Teste com Falso 
+		// 2Â° Teste com Falso 
 		pilhaMath.addArray(textoIncorreto1);
 		assertFalse(" Texto correto! ", pilhaMath.verifyMath());
 		pilhaMath.clear();
 		
-		// 3° Teste com Falso 
+		// 3Â° Teste com Falso 
 		pilhaMath.addArray(textoIncorreto2);
 		assertFalse(" Texto correto! ", pilhaMath.verifyMath());
 		pilhaMath.clear();
 		
-		// 4° Teste com Falso 
+		// 4Â° Teste com Falso 
 		pilhaMath.addArray(textoIncorreto3);
 		assertFalse(" Texto correto! ", pilhaMath.verifyMath());
 		pilhaMath.clear();
 		
-		// 5° Teste com Falso 
+		// 5Â° Teste com Falso 
 		pilhaMath.addArray(textoIncorreto4);
 		assertFalse(" Texto correto! ", pilhaMath.verifyMath());
 		pilhaMath.clear();
